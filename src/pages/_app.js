0982756213016b40740fc03534b819db66c4d887
Faisal Ahmed <92742016+faisalahmed11@users.noreply.github.com>
2023-05-21 15:38:54 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,8 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return <>
 <Head>
-        <title>RWD-template-1</title>
-        <meta name="description" content="This is a responsive web design template" />
+        <title>Responsive-Band-Site</title>
+        <meta name="description" content="This is a responsive band website " />
         
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
